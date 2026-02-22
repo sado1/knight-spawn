9ea82bed025ccd30510f36f85f469a91a2f1690b
Grzegorz Sadowski <sado1@mailbox.org>
2026-02-22 16:12:31 +0100

    create systemd unit and run server once to generate INI

--- generator.py
+++ generator.py
@@ -14,7 +14,44 @@ class read_configuration:
             'KMR': "KaM Remake Server Settings.ini",
             'KP': "KnightsProvince_Settings.ini"
             }
+        self.GAME_FULL_NAME = {
+            'KMR': "KaM Remake",
+            'KP': "Knights Province"
+            }
+        self.GAME_BINARY_NAME = {
+            'KMR': "KaM_Remake_Server_linux_x86_64",
+            'KP': "KP_DedicatedServer_Linux_x64"
+            }
+        self.SYSTEMD_ALL_UNITS_PATH = "/usr/lib/systemd/system"
+        self.SYSTEMD_UNIT_NAME = self.GAME + "-" + self.INSTANCE_NAME + ".service"
+        self.SYSTEMD_UNIT_PATH = self.SYSTEMD_ALL_UNITS_PATH + "/" + self.SYSTEMD_UNIT_NAME
+        self.SYSTEMD_UNIT_CONTENT = '''[Unit]
+Description=''' + self.GAME_FULL_NAME[self.GAME] + " " + self.INSTANCE_NAME + ''' game server
+After=network.target
 
+[Service]
+WorkingDirectory=''' + self.SRV_PATH + '''
+User=''' + self.SRV_USER + '''
+Group=''' + self.SRV_USER + '''
+Type=simple
+ExecStart=''' + self.SRV_PATH + "/" + self.GAME_BINARY_NAME[self.GAME] + '''
+RestartSec=15
+Restart=always
+
+[Install]
+WantedBy=multi-user.target'''
+
+    def return_INI_for_game(self):
+        if self.GAME == 'KMR':
+            pass
+        elif self.GAME == 'KP':
+            pass
+        '''
+ServerName='REPLACE_SERVER_NAME'
+WelcomeMessage=REPLACE_WELCOME_MESSAGE
+ServerPort=REPLACE_SERVER_PORT
+UDPScanPort=REPLACE_SERVER_PORT_2
+        '''
 
     def generate_instructions(self):
         # print("apt update\napt install " + self.to_install)
@@ -23,7 +60,11 @@ class read_configuration:
         print("curl -L " + self.ARCHIVE_URL + " -o " + self.SRV_PATH + "/" + self.SRV_FILENAME)
         print("cd " + self.SRV_PATH )
         print("7z x " + self.SRV_FILENAME  + " " + self.SRV_PATH)
-        systemd_unit = '''file
-content
-here'''
-        print("[MANUAL] xxx")
+        print("chmod +x " + self.GAME_BINARY_NAME[self.GAME])
+        print("echo '" + self.SYSTEMD_UNIT_CONTENT  + "' > " + self.SYSTEMD_UNIT_PATH + "\n\nchmod +x " + self.SYSTEMD_UNIT_PATH + "\n")
+        print("systemctl start " + self.SYSTEMD_UNIT_NAME + "; sleep 3; systemctl stop " + self.SYSTEMD_UNIT_NAME)
+
+'''
+TODO
+- start server, sleep 5, stop server, replace INI settings, start server
+'''
