3a7b8a78c8858eccd48fe2cf18b3cfb19c6513f3
Grzegorz Sadowski <sado1@mailbox.org>
2026-02-22 15:48:34 +0100

    download and extract dedicated server archive

--- generator.py
+++ generator.py
@@ -1,14 +1,30 @@
 
 class read_configuration:
-    def __init__(self, USERNAME, KMR_URL, SRV_NAME):
-        self.USERNAME = USERNAME
-        self.KMR_URL = KMR_URL
-        self.KMR_INSTANCE_NAME = SRV_NAME
+    def __init__(self, SRV_USER, ARCHIVE_URL, SRV_NAME, GAME):
+        self.SRV_USER = SRV_USER
+        self.ARCHIVE_URL = ARCHIVE_URL
+        self.INSTANCE_NAME = SRV_NAME
+        self.GAME = GAME
+
+        self.SRV_FILENAME = ARCHIVE_URL.split('/')[-1]
+        self.SRV_HOME_PATH = "/home/" + self.SRV_USER
+        self.SRV_PATH = self.SRV_HOME_PATH + "/" + self.INSTANCE_NAME
         self.to_install = "xxx"
+        self.SETTINGS_FILE_NAME = {
+            'KMR': "KaM Remake Server Settings.ini",
+            'KP': "KnightsProvince_Settings.ini"
+            }
 
 
     def generate_instructions(self):
-        print("apt update\napt install " + self.to_install)
+        # print("apt update\napt install " + self.to_install)
+        print("useradd " + self.SRV_USER + " -m " + self.SRV_HOME_PATH )
+        print("mkdir -p " + self.SRV_PATH)
+        print("curl -L " + self.ARCHIVE_URL + " -o " + self.SRV_PATH + "/" + self.SRV_FILENAME)
+        # https://api.beeper.com/desktop/download/linux/" + beeper_arch + "/stable/com.automattic.beeper.desktop -o ~/Pobrane/beeper.AppImage
+        print("cd " + self.SRV_PATH )
+        # wget self.ARCHIVE_URL
+        print("7z x " + self.SRV_FILENAME  + " " + self.SRV_PATH)
         systemd_unit = '''file
 content
 here'''
--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 import sys
 import generator
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     print("Error - you need 3 arguments and this is what you provided: " + str(sys.argv[1:]))
-    print("username")
-    print("KMR URL")
-    print("SRV_NAME")
+    print("SRV_USER which will run the server")
+    print("ARCHIVE_URL to dedicated server archive")
+    print("INSTANCE_NAME - name for server directory/service ie. kp_r12345 - you can deploy multiple versions that way")
+    print("GAME - KMR or KP")
     exit(1)
 else:
-    c1 = generator.read_configuration(sys.argv[1], sys.argv[2], sys.argv[3])
+    c1 = generator.read_configuration(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     c1.generate_instructions()
     print("Done.")
